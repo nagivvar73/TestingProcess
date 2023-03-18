119cf85f643c30446eb7479eec06660ece09b466
nagivvar73 <nvvarao@gmail.com>
2023-03-18 11:08:03 +0530

    Commit-2

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm Changing In Main - Rebase Test')
-      print_hi('PyCharm Changing In Main - Rebase Test3')
+    print_hi('PyCharm Changing In Main - Rebase Test3')
 
  
