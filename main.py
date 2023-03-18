275970571de9aaf36ae3b4b90ff6524346a9f36d
nagivvar73 <46120370+nagivvar73@users.noreply.github.com>
2023-03-18 13:27:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def print_hi(name):
 def print_hi1(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    
+def print_hi2(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
